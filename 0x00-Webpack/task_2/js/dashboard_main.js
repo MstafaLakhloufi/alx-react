aee9d5b6857fe07e707ab90c6bb4608bf41eb8f5
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-11-11 16:17:50 +0100

    webpack

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,8 +1,7 @@
 
-'use strict';
-import '../css/main.css';
 const $ = require('jquery');
 const _ = require('lodash');
+import '../css/main.css';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -10,13 +9,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-const updateCounter = () => {
-  let clicks = $('#count').html() || 0;
-  $('button').on('click', () => {
-    clicks++;
-    $('#count').html(`${clicks} clicks on the button`);
-  });
-};
+let count = 0;
 
-_.debounce(updateCounter, 500);
-updateCounter();
+function updateCounter() {
+  count++;
+  $('#count').html(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
