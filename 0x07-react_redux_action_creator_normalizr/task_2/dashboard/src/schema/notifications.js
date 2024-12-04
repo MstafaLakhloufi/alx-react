b3e12804d09ba8b5e4d1620833d0ca48685f5980
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-12-04 15:57:49 +0100

    Filter a normalized Schema

--- 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -1,34 +1,30 @@
-import * as notificationData from '../../../../notifications.json';
-import { normalize, schema } from 'normalizr'; 
-
-export function getAllNotificationsByUser(userId) {
-    const notifications = normalizedData.entities.notifications;
-    const messages = normalizedData.entities.messages;
-
-    const result = [];
-
-    for (const id in notifications) {
-        if(notifications[id].author === userId) {
-            result.push(messages[notifications[id].context]);
-        }
-    }
-    return result;
-}
+import * as notificationData from "../../notifications.json";
+import { normalize, schema } from "normalizr";
 
 const user = new schema.Entity("users");
-const message = new schema.Entity(
-    "messages",
-    {},
-    {
-        idAttribute: "guid",
-    }
-);
+
+const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
 
 const notification = new schema.Entity("notifications", {
-    author: user,
-    context: message,
+  author: user,
+  context: message,
 });
 
-const normalizedData = normalize(notificationsData.default, [notification]);
+const normalizedData = normalize(notificationData.default, [notification]);
 
 export { normalizedData };
+
+export function getAllNotificationsByUser(userId) {
+  const notifications = normalizedData.entities.notifications;
+  const messages = normalizedData.entities.messages;
+
+  const result = [];
+
+  for (const id in notifications) {
+    if (notifications[id].author === userId) {
+      result.push(messages[notifications[id].context]);
+    }
+  }
+
+  return result;
+}
