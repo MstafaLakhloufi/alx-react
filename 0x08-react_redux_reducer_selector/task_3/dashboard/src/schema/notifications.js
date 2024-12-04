c429652e8a7a3f5cc0198e3147b372aaf63cb109
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-12-04 17:21:31 +0100

    react redux reducer selector

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/schema/notifications.js
@@ -0,0 +1,34 @@
+import * as notificationsData from '../../../../notifications.json';
+import { normalize, schema } from 'normalizr'; 
+
+export function getAllNotificationsByUser(userId) {
+    const notifications = normalizedData.entities.notifications;
+    const messages = normalizedData.entities.messages;
+
+    const result = [];
+
+    for (const id in notifications) {
+        if(notifications[id].author === userId) {
+            result.push(messages[notifications[id].context]);
+        }
+    }
+    return result;
+}
+
+const user = new schema.Entity("users");
+const message = new schema.Entity(
+    "messages",
+    {},
+    {
+        idAttribute: "guid",
+    }
+);
+
+const notification = new schema.Entity("notifications", {
+    author: user,
+    context: message,
+});
+
+const normalizedData = normalize(notificationsData.default, [notification]);
+
+export { normalizedData };
